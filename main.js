d904980703a9f83d83781bd20fbdb99e0bb49a26
arcox80 <drew.r.cox@gmail.com>
2022-05-06 18:51:51 -0400

    Added style sheet and eventlistener on button.

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-// your code here
+const button = document.getElementsByTagName('button')[0];
+button.addEventListener('click', function () {
+  const div = document.createElement('div');
+  div.classList.add('page-header');
+  const p1 = document.createElement('p');
+  const p2 = document.createElement('p');
+  const strong = document.createElement('strong');
+  strong.appendChild(document.createTextNode(document.getElementById('name').value));
+  const postMessage = document.createTextNode(document.getElementById('message').value);
+  const byAuthor = document.createTextNode('Posted By: ');
+  const target = document.getElementsByClassName('posts')[0];
+  p1.appendChild(postMessage);
+  p2.append(byAuthor, strong);
+  div.append(p1, p2);
+  target.appendChild(div);
+});
