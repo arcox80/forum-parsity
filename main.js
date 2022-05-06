4d9096d921d4aceb2764b3ed9a6969cf32a3d2ca
arcox80 <drew.r.cox@gmail.com>
2022-05-06 19:34:00 -0400

    Removed style.css and reformated eventlistener

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
-const button = document.getElementsByTagName('button')[0];
-button.addEventListener('click', function () {
-  const div = document.createElement('div');
-  div.classList.add('page-header');
-  const p1 = document.createElement('p');
-  const p2 = document.createElement('p');
-  const strong = document.createElement('strong');
-  strong.appendChild(document.createTextNode(document.getElementById('name').value));
-  const postMessage = document.createTextNode(document.getElementById('message').value);
-  const byAuthor = document.createTextNode('Posted By: ');
+document.getElementById('submit').addEventListener('click', function () {
   const target = document.getElementsByClassName('posts')[0];
-  p1.appendChild(postMessage);
-  p2.append(byAuthor, strong);
-  div.append(p1, p2);
+  const div = document.createElement('div');
+  const author = document.getElementById('name').value;
+  const message = document.getElementById('message').value;
+
+  div.innerHTML = 
+    `<p>${message}</p>
+    <p>Posted by: <strong>${author}</strong></p>
+    <hr>`;
+
   target.appendChild(div);
 });
